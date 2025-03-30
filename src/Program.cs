4076543a4342c6a383131aa3ff2e86b24fd3f3d2
Greg Whiteley <greg.whiteley@gmail.com>
2025-03-30 17:12:56 +1100

    proof of concept can talk to ENVDTE100

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,5 @@
+// (C) Copyright 2025 Greg Whiteley
+
 internal class Program
 {
     private static void Main(string[] args)
--- src/env.cs
+++ src/env.cs
@@ -1,31 +1,78 @@
+// (C) Copyright 2025 Greg Whiteley
+using System.Runtime.Versioning;
 
 namespace VsTool
 {
-    //public
+    #region Version Interface
+    // For now these are just direct wrappers of functionality
+    // the idea is to implement the pieces of larger bits of functionality
+    // overloaded by supported version
+    interface Solution
+    {
+        void Build(bool Wait = false);
+        void Stop(bool Wait = false);
+        void Go(bool Wait = false);
+    };
+
+    [SupportedOSPlatform("windows")]
+    class DTE100 : Solution
+    {
+        public DTE100(EnvDTE100.Solution4 sln)
+        {
+            m_sln = sln;
+        }
+
+        EnvDTE100.Solution4 m_sln;
+
+        void Solution.Build(bool wait)
+        {
+            m_sln.SolutionBuild.Build(wait);
+        }
+
+        void Solution.Stop(bool wait)
+        {
+            m_sln.DTE.Debugger.Stop(wait);
+        }
+
+        void Solution.Go(bool wait)
+        {
+            m_sln.DTE.Debugger.Go(wait);
+        }
+    };
+    #endregion
+
     public class Env
     {
     #region Ctor & Destructor
-        /// <summary>
-        /// Standard Constructor
-        /// </summary>
         public Env(string path)
         {
             m_path = path;
 
-            var p = System.Runtime.InteropServices.Marshal.BindToMoniker(path);
-            System.Console.WriteLine("{}", p);
+            object? p = null;
+            if (OperatingSystem.IsWindows())
+            {
+                p = System.Runtime.InteropServices.Marshal.BindToMoniker(path);
 
+                if ((EnvDTE100.Solution4?)p is EnvDTE100.Solution4 e100)
+                {
+                    m_solution = new DTE100(e100);
+                    return;
+                }
+            }
+            throw new TypeLoadException($"Unable to load matching type for solution: {p}");
         }
 
-        /// <summary>
-        /// Default Destructor
-        /// </summary>
         ~Env()
         {
         }
-    #endregion
+        #endregion
+
+        public void Build(bool Wait = false) => m_solution.Build(Wait);
+        public void Stop(bool Wait = false) => m_solution.Stop(Wait);
+        public void Go(bool Wait = false) => m_solution.Go(Wait);
 
         private string m_path;
+        private Solution m_solution;
     }
 
 }
