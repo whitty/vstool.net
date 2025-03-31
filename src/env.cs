b7becddeca2ce8ebe572d612db55e8c650d21c6b
Greg Whiteley <greg.whiteley@gmail.com>
2025-03-31 13:17:06 +1100

    Realise this isn't C++ and use namespace declarations

--- src/env.cs
+++ src/env.cs
@@ -2,133 +2,131 @@
 // (C) Copyright 2025 Greg Whiteley
 using System.Runtime.Versioning;
 
-namespace VsTool
+namespace VsTool;
+
+#region Version Interface
+// For now these are just direct wrappers of functionality
+// the idea is to implement the pieces of larger bits of functionality
+// overloaded by supported version
+interface Solution
 {
-    #region Version Interface
-    // For now these are just direct wrappers of functionality
-    // the idea is to implement the pieces of larger bits of functionality
-    // overloaded by supported version
-    interface Solution
+    void Build(bool Wait = false);
+    void Stop(bool Wait = false);
+    void Go(bool Wait = false);
+};
+
+[SupportedOSPlatform("windows")]
+sealed class DTE100 : Solution
+{
+    public DTE100(EnvDTE100.Solution4 sln)
     {
-        void Build(bool Wait = false);
-        void Stop(bool Wait = false);
-        void Go(bool Wait = false);
-    };
-
-    [SupportedOSPlatform("windows")]
-    sealed class DTE100 : Solution
-    {
-        public DTE100(EnvDTE100.Solution4 sln)
-        {
-            m_sln = sln;
-        }
-
-        EnvDTE100.Solution4 m_sln;
-
-        void Solution.Build(bool wait)
-        {
-            m_sln.SolutionBuild.Build(wait);
-        }
-
-        void Solution.Stop(bool wait)
-        {
-            m_sln.DTE.Debugger.Stop(wait);
-        }
-
-        void Solution.Go(bool wait)
-        {
-            m_sln.DTE.Debugger.Go(wait);
-        }
-    };
-
-    [SupportedOSPlatform("windows")]
-    sealed class DTE80 : Solution
-    {
-        public DTE80(EnvDTE80.DTE2 sln)
-        {
-            m_sln = sln;
-        }
-
-        EnvDTE80.DTE2 m_sln;
-
-        void Solution.Build(bool wait)
-        {
-            m_sln.Solution.SolutionBuild.Build(wait);
-        }
-
-        void Solution.Stop(bool wait)
-        {
-            m_sln.DTE.Debugger.Stop(wait);
-        }
-
-        void Solution.Go(bool wait)
-        {
-            m_sln.DTE.Debugger.Go(wait);
-        }
-    };
-
-    #endregion
-
-    public sealed class NotRunningException : Exception
-    {
-        public NotRunningException(string path) : base($"'{path}' is not running") {}
-        public NotRunningException(string path, Exception inner) : base($"'{path}' is not running", inner) {}
-        private NotRunningException() : base() {}
+        m_sln = sln;
     }
 
-    public class Env
-    {
-    #region Ctor & Destructor
-        public Env(string path)
-        {
-            ArgumentNullException.ThrowIfNull(path);
-            m_path = Path.GetFullPath(path);
+    EnvDTE100.Solution4 m_sln;
 
-            object? p = null;
-            if (OperatingSystem.IsWindows())
+    void Solution.Build(bool wait)
+    {
+        m_sln.SolutionBuild.Build(wait);
+    }
+
+    void Solution.Stop(bool wait)
+    {
+        m_sln.DTE.Debugger.Stop(wait);
+    }
+
+    void Solution.Go(bool wait)
+    {
+        m_sln.DTE.Debugger.Go(wait);
+    }
+};
+
+[SupportedOSPlatform("windows")]
+sealed class DTE80 : Solution
+{
+    public DTE80(EnvDTE80.DTE2 sln)
+    {
+        m_sln = sln;
+    }
+
+    EnvDTE80.DTE2 m_sln;
+
+    void Solution.Build(bool wait)
+    {
+        m_sln.Solution.SolutionBuild.Build(wait);
+    }
+
+    void Solution.Stop(bool wait)
+    {
+        m_sln.DTE.Debugger.Stop(wait);
+    }
+
+    void Solution.Go(bool wait)
+    {
+        m_sln.DTE.Debugger.Go(wait);
+    }
+};
+
+#endregion
+
+public sealed class NotRunningException : Exception
+{
+    public NotRunningException(string path) : base($"'{path}' is not running") {}
+    public NotRunningException(string path, Exception inner) : base($"'{path}' is not running", inner) {}
+    private NotRunningException() : base() {}
+}
+
+public class Env
+{
+#region Ctor & Destructor
+    public Env(string path)
+    {
+        ArgumentNullException.ThrowIfNull(path);
+        m_path = Path.GetFullPath(path);
+
+        object? p = null;
+        if (OperatingSystem.IsWindows())
+        {
+            if (!ROT.IsRunning(m_path))
             {
-                if (!ROT.IsRunning(m_path))
-                {
-                    // TODO in admin mode this sometimes fails
+                // TODO in admin mode this sometimes fails
 
-                    bool running = false;
-                    foreach (var v in ROT.EachRunning())
+                bool running = false;
+                foreach (var v in ROT.EachRunning())
+                {
+                    if (v == m_path)
                     {
-                        if (v == m_path)
-                        {
-                            running = true;
-                            break;
-                        }
+                        running = true;
+                        break;
                     }
-
-                    if (!running)
-                        throw new NotRunningException(m_path);
                 }
 
-                p = System.Runtime.InteropServices.Marshal.BindToMoniker(m_path);
-
-                if ((EnvDTE100.Solution4?)p is EnvDTE100.Solution4 e100)
-                {
-                    m_solution = new DTE100(e100);
-                    return;
-                }
-
-                if ((EnvDTE80.DTE2?)p is EnvDTE80.DTE2 e80)
-                {
-                    m_solution = new DTE80(e80);
-                    return;
-                }
+                if (!running)
+                    throw new NotRunningException(m_path);
             }
-            throw new TypeLoadException($"Unable to load matching type for solution: {p}");
+
+            p = System.Runtime.InteropServices.Marshal.BindToMoniker(m_path);
+
+            if ((EnvDTE100.Solution4?)p is EnvDTE100.Solution4 e100)
+            {
+                m_solution = new DTE100(e100);
+                return;
+            }
+
+            if ((EnvDTE80.DTE2?)p is EnvDTE80.DTE2 e80)
+            {
+                m_solution = new DTE80(e80);
+                return;
+            }
         }
-        #endregion
-
-        public void Build(bool Wait = false) => m_solution.Build(Wait);
-        public void Stop(bool Wait = false) => m_solution.Stop(Wait);
-        public void Go(bool Wait = false) => m_solution.Go(Wait);
-
-        private string m_path;
-        private Solution m_solution;
+        throw new TypeLoadException($"Unable to load matching type for solution: {p}");
     }
+#endregion
 
+    public void Build(bool Wait = false) => m_solution.Build(Wait);
+    public void Stop(bool Wait = false) => m_solution.Stop(Wait);
+    public void Go(bool Wait = false) => m_solution.Go(Wait);
+
+    private string m_path;
+    private Solution m_solution;
 }
--- src/rot.cs
+++ src/rot.cs
@@ -5,144 +5,142 @@ using Microsoft.VisualStudio.OLE.Interop;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 
-namespace VsTool
+namespace VsTool;
+
+[SupportedOSPlatform("windows")]
+internal static class ROT
 {
-    [SupportedOSPlatform("windows")]
-    internal static class ROT
+    public sealed class COMException: System.Runtime.InteropServices.COMException
     {
-        public sealed class COMException: System.Runtime.InteropServices.COMException
+        public COMException(string? message, int errorCode) : base(message, errorCode) {}
+        public COMException(string? message) : base(message) {}
+        public COMException(string? message, Exception inner) : base(message, inner) {}
+        private COMException() : base() {}
+    }
+
+    [DllImport("ole32.dll")]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+
+    [DllImport("ole32.dll")]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static extern int GetRunningObjectTable(uint reserved, out IRunningObjectTable pprot);
+
+    [DllImport("ole32.dll")]
+    [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
+    private static extern int MkParseDisplayName(IBindCtx pbc, [MarshalAs(UnmanagedType.LPWStr)] String szUserName, out UInt32 pchEaten, out IMoniker ppmk);
+
+    static IRunningObjectTable GetRot()
+    {
+        IRunningObjectTable? pRunningObjectTable = null;
+        var hResult = GetRunningObjectTable(0, out pRunningObjectTable);
+        if (hResult < 0)
         {
-            public COMException(string? message, int errorCode) : base(message, errorCode) {}
-            public COMException(string? message) : base(message) {}
-            public COMException(string? message, Exception inner) : base(message, inner) {}
-            private COMException() : base() {}
+            Marshal.ThrowExceptionForHR(hResult);
+        }
+        if (pRunningObjectTable == null)
+        {
+            throw new COMException("Failed to get RunningObjectTable", hResult);
+        }
+        return pRunningObjectTable;
+    }
+
+    static IBindCtx CreateBindContext()
+    {                        IBindCtx? pBindCtx = null;
+        var hResult = CreateBindCtx(0, out pBindCtx);
+        if (hResult < 0)
+        {
+            Marshal.ThrowExceptionForHR(hResult);
+        }
+        if (pBindCtx == null)
+        {
+            throw new COMException("Failed to get BindCtx", hResult);
         }
 
-        [DllImport("ole32.dll")]
-        [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
-        private static extern int CreateBindCtx(uint reserved, out IBindCtx ppbc);
+        return pBindCtx;
+    }
 
-        [DllImport("ole32.dll")]
-        [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
-        private static extern int GetRunningObjectTable(uint reserved, out IRunningObjectTable pprot);
-
-        [DllImport("ole32.dll")]
-        [DefaultDllImportSearchPaths(DllImportSearchPath.UserDirectories)]
-        private static extern int MkParseDisplayName(IBindCtx pbc, [MarshalAs(UnmanagedType.LPWStr)] String szUserName, out UInt32 pchEaten, out IMoniker ppmk);
-
-        static IRunningObjectTable GetRot()
+    static IMoniker ParseDisplayName(IBindCtx pBindCtx, string mk)
+    {
+        UInt32 cbEaten;
+        IMoniker? pMoniker = null;
+        var hResult = MkParseDisplayName(pBindCtx, mk, out cbEaten, out pMoniker);
+        if (hResult < 0)
         {
-            IRunningObjectTable? pRunningObjectTable = null;
-            var hResult = GetRunningObjectTable(0, out pRunningObjectTable);
-            if (hResult < 0)
-            {
-                Marshal.ThrowExceptionForHR(hResult);
-            }
-            if (pRunningObjectTable == null)
-            {
-                throw new COMException("Failed to get RunningObjectTable", hResult);
-            }
-            return pRunningObjectTable;
+            Marshal.ThrowExceptionForHR(hResult);
+        }
+        if (cbEaten < mk.Length || pMoniker == null)
+        {
+            throw new COMException($"Failed to interpret '{mk}' as moniker");
+        }
+        return pMoniker;
+    }
+
+    public static bool IsRunning(string path)
+    {
+        IRunningObjectTable pRunningObjectTable = GetRot();
+        IBindCtx pBindCtx = CreateBindContext();
+
+        IMoniker pMoniker = ParseDisplayName(pBindCtx, path);
+
+        var hResult = pRunningObjectTable.IsRunning(pMoniker);
+        if (hResult < 0)
+        {
+            Marshal.ThrowExceptionForHR(hResult);
         }
 
-        static IBindCtx CreateBindContext()
-        {                        IBindCtx? pBindCtx = null;
-            var hResult = CreateBindCtx(0, out pBindCtx);
-            if (hResult < 0)
-            {
-                Marshal.ThrowExceptionForHR(hResult);
-            }
-            if (pBindCtx == null)
-            {
-                throw new COMException("Failed to get BindCtx", hResult);
-            }
+        return hResult == 0;
+    }
 
-            return pBindCtx;
+    public static System.Collections.Generic.IEnumerable<string> EachRunning()
+    {
+        IBindCtx pBindCtx = CreateBindContext();
+
+        IRunningObjectTable pRunningObjectTable = GetRot();
+
+        IEnumMoniker? pEnumMoniker = null;
+        pRunningObjectTable.EnumRunning(out pEnumMoniker);
+        if (pEnumMoniker is null)
+        {
+            throw new COMException("Failed to get EnumRunning");
         }
 
-        static IMoniker ParseDisplayName(IBindCtx pBindCtx, string mk)
+        pEnumMoniker.Reset();
+
+        IMoniker[] monikers = new IMoniker[1];
+        uint fetched = 0;
+        var hResult = pEnumMoniker.Next((uint)monikers.Length, monikers, out fetched);
+
+        while (hResult == 0)
         {
-            UInt32 cbEaten;
-            IMoniker? pMoniker = null;
-            var hResult = MkParseDisplayName(pBindCtx, mk, out cbEaten, out pMoniker);
-            if (hResult < 0)
+            string s = "";
+            monikers[0].GetDisplayName(pBindCtx, monikers[0], out s);
+
+            // Confirm that DisplayName is parseable so could be matched
+            if (s.Length > 0)
             {
-                Marshal.ThrowExceptionForHR(hResult);
-            }
-            if (cbEaten < mk.Length || pMoniker == null)
-            {
-                throw new COMException($"Failed to interpret '{mk}' as moniker");
-            }
-            return pMoniker;
-        }
-
-        public static bool IsRunning(string path)
-        {
-            IRunningObjectTable pRunningObjectTable = GetRot();
-            IBindCtx pBindCtx = CreateBindContext();
-
-            IMoniker pMoniker = ParseDisplayName(pBindCtx, path);
-
-            var hResult = pRunningObjectTable.IsRunning(pMoniker);
-            if (hResult < 0)
-            {
-                Marshal.ThrowExceptionForHR(hResult);
-            }
-
-            return hResult == 0;
-        }
-
-        public static System.Collections.Generic.IEnumerable<string> EachRunning()
-        {
-            IBindCtx pBindCtx = CreateBindContext();
-
-            IRunningObjectTable pRunningObjectTable = GetRot();
-
-            IEnumMoniker? pEnumMoniker = null;
-            pRunningObjectTable.EnumRunning(out pEnumMoniker);
-            if (pEnumMoniker is null)
-            {
-                throw new COMException("Failed to get EnumRunning");
-            }
-
-            pEnumMoniker.Reset();
-
-            IMoniker[] monikers = new IMoniker[1];
-            uint fetched = 0;
-            var hResult = pEnumMoniker.Next((uint)monikers.Length, monikers, out fetched);
-
-            while (hResult == 0)
-            {
-                string s = "";
-                monikers[0].GetDisplayName(pBindCtx, monikers[0], out s);
-
-                // Confirm that DisplayName is parseable so could be matched
-                if (s.Length > 0)
+                try
                 {
-                    try
-                    {
-                        if (ParseDisplayName(pBindCtx, s) == null)
-                        {
-                            s = "";
-                        }
-                    }
-                    catch (COMException)
-                    {
-                        s = "";
-                    }
-                    catch (System.Runtime.InteropServices.COMException)
+                    if (ParseDisplayName(pBindCtx, s) == null)
                     {
                         s = "";
                     }
                 }
-                if (s.Length > 0)
+                catch (COMException)
                 {
-                    yield return s;
+                    s = "";
                 }
-                hResult = pEnumMoniker.Next((uint)monikers.Length, monikers, out fetched);
+                catch (System.Runtime.InteropServices.COMException)
+                {
+                    s = "";
+                }
             }
+            if (s.Length > 0)
+            {
+                yield return s;
+            }
+            hResult = pEnumMoniker.Next((uint)monikers.Length, monikers, out fetched);
         }
+    }
 
-    } // ROT
-
-} // VsTool
+} // ROT
